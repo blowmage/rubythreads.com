ba2602cf4784fc254fd81aca02a82f6ad90838bd
Mike Moore <mike@blowmage.com>
2012-10-01 14:55:17 -0600

    Convert old site from Shopify to Jekyll

---
+++ javascripts/rubythreads.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	// Clicking on main logo should send user home...
+	$('body>header h1').click(function() {
+		document.location = $('body>header h1 a').attr('href');
+		return true;
+	});
+
+	$('section#banners').cycle({
+		pause:   1,
+    	speed:   250,
+    	timeout: 10000
+	});
+
+	//$('section#product div#product-images a').imgbox({
+	//	'zoomOpacity' : true,
+	//	'alignment'	  : 'center'
+	//});
+});
